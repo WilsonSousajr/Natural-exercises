c01d3baa493b285a8158accb61030f765786621b
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-19 18:17:34 -0300

    Create Basic-2 folder and create remove-duplicated-73.py file

---
+++ Python/Basic-2/remove-duplicated-73.py
@@ -0,0 +1,7 @@
+# 73. Write a Python program that removes duplicate elements from a given array of numbers so that each element appears only once and returns the new length of the array.
+# Sample Input:
+# [0,0,1,1,2,2,3,3,4,4,4]
+# [1, 2, 2, 3, 4, 4]
+# Sample Output:
+# 5
+# 4
--- Python/concat-everything-27.py
+++
@@ -1,9 +0,0 @@
-# Write a Python program that concatenates all elements in a list into a string and returns it.
-
-list1 = ['H','e','l','l','o',' ','W','o','r','l','d','!']
-
-def returnConcatenated(list):
-    resul = ''.join(list)
-    return resul
-
-print(returnConcatenated(list1))
