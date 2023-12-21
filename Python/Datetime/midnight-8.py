3e95295caca81b7ecb2c71610670aab7d83d0943
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-21 18:20:29 -0300

    Solve exercise 8 from Datetime folder

---
+++ Python/Datetime/midnight-8.py
@@ -0,0 +1,7 @@
+#Write a Python program to convert the date to datetime (midnight of the date) in Python.
+#Sample Output : 2015-06-22 00:00:00
+
+from datetime import date, datetime
+
+today = date.today()
+print(datetime.combine(today, datetime.min.time()))
