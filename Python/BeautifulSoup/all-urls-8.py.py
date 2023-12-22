a3da45ec32f602728839b9f144113ad3273d9d4b
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-22 13:14:39 -0300

    feat: Create BeautifulSoup folder and create its exercises files

---
+++ Python/BeautifulSoup/all-urls-8.py.py
@@ -0,0 +1,2 @@
+# Write a Python program to extract all the URLs from the webpage python.org 
+# that are nested within <li> tags from .
---
+++ Python/BeautifulSoup/paragraph-number-3.py
@@ -0,0 +1,3 @@
+# Write a Python program to get the number of paragraph tags of a given html document.
+
+
