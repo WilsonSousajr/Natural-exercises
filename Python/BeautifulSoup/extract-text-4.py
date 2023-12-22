c7bb7698d1a0eb0d862d66eafa40e76f50eec3db
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-22 15:32:03 -0300

    feat: Solve exercise 4 from BeautifulSoup folder

--- Python/BeautifulSoup/extract-text-4.py
+++ Python/BeautifulSoup/extract-text-4.py
@@ -1,1 +1,13 @@
 # Write a Python program to extract the text in the first paragraph tag of a given html document.
+# Output: All our teachers are university-educated, native speakers of the language they teach 
+# and undergo rigorous training in the Natural methodology. Find out more about them:
+
+import requests
+from bs4 import BeautifulSoup
+
+link = "https://www.linguanaturalis.com/"
+request = requests.get(link)
+html_document = BeautifulSoup(request.text, "html.parser")
+text_in_the_first_paragraph = html_document.p.string
+
+print(text_in_the_first_paragraph)
