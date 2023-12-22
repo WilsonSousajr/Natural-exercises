bc5ed062aefd81a651b97edaf6c50af1d472a2db
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-22 15:32:25 -0300

    feat: Solve exercise 29 from BeautifulSoup folder

--- Python/BeautifulSoup/print-id-19.py
+++ Python/BeautifulSoup/print-id-19.py
@@ -1,1 +1,11 @@
 # Write a Python program to print the element(s) that has a specified id of a given web page.
+
+import requests
+from bs4 import BeautifulSoup
+
+link = "https://www.linguanaturalis.com/"
+request = requests.get(link)
+html_document = BeautifulSoup(request.text, "html.parser")
+element_id = '#main_navbar'
+
+print(html_document.select_one(element_id))
