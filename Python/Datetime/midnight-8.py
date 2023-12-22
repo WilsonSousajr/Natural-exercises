7b80a10196f7e1e6baf38cad18a336fdc6c0fe8c
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-22 13:20:37 -0300

    style: Fix the number of characters in commented lines

--- Python/Datetime/midnight-8.py
+++ Python/Datetime/midnight-8.py
@@ -1,5 +1,5 @@
-#Write a Python program to convert the date to datetime (midnight of the date) in Python.
-#Sample Output : 2015-06-22 00:00:00
+# Write a Python program to convert the date to datetime (midnight of the date) in Python.
+# Sample Output : 2015-06-22 00:00:00
 
 from datetime import date, datetime
 
