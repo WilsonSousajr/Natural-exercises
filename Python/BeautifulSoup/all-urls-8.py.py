c7a7bacbfe6db7200a212b8f036eb3bc56901091
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-22 15:31:26 -0300

    feat: Solve exercise 8 from BeautifulSoup folder

--- Python/BeautifulSoup/all-urls-8.py.py
+++ Python/BeautifulSoup/all-urls-8.py.py
@@ -1,2 +1,24 @@
 # Write a Python program to extract all the URLs from the webpage python.org 
 # that are nested within <li> tags from .
+# Output: ['#', '/en/natural/method/', '#', '#', '/en/natural/contact/', '#', 
+# '/en/natural/courses/english/#teachers', '/en/natural/courses/english/']
+
+import requests
+from bs4 import BeautifulSoup
+
+link = "https://www.linguanaturalis.com/"
+request = requests.get(link)
+urls = []
+html_document = BeautifulSoup(request.text, "html.parser")
+
+for links in html_document.find_all('li'):
+    a = links.find('a')
+    try:
+        if 'href' in a.attrs:
+            urls.append(a.attrs['href'])
+    except:
+        pass
+    
+print(urls)
+
+
