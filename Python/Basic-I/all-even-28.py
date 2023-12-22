edbfa26f549fa5c9446b64c7932afa255c775341
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-22 13:18:00 -0300

    style: Fix the number of characters in commented lines

--- Python/Basic-I/all-even-28.py
+++ Python/Basic-I/all-even-28.py
@@ -1,4 +1,5 @@
-# Write a Python program to print all even numbers from a given list of numbers in the same order and # stop printing any after 237 in the sequence.
+# Write a Python program to print all even numbers from a given list of numbers in the same 
+# order and stop printing any after 237 in the sequence.
 # Sample numbers list :
 
 # numbers = [    
