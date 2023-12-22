9cc4b16660e8dd70de842e7cdea6ebc6baeb9272
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-22 13:20:14 -0300

    style: Fix the number of characters in commented lines

--- Python/Basic-2/remove-duplicated-73.py
+++ Python/Basic-2/remove-duplicated-73.py
@@ -1,4 +1,5 @@
-# 73. Write a Python program that removes duplicate elements from a given array of numbers so that each element appears only once and returns the new length of the array.
+# 73. Write a Python program that removes duplicate elements from a given array of numbers 
+# so that each element appears only once and returns the new length of the array.
 # Sample Input:
 # [0,0,1,1,2,2,3,3,4,4,4]
 # [1, 2, 2, 3, 4, 4]
