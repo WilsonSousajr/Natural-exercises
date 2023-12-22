b686620fa8b649e66c5e468f8090a9ca4f3392b4
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-22 15:30:33 -0300

    feat: Solve exercise 3 from BeautifulSoup folder

--- Python/BeautifulSoup/paragraph-number-3.py
+++ Python/BeautifulSoup/paragraph-number-3.py
@@ -1,3 +1,12 @@
 # Write a Python program to get the number of paragraph tags of a given html document.
+# Output: Number of paragraph tags: 33
 
+import requests
+from bs4 import BeautifulSoup
 
+link = "https://www.linguanaturalis.com/"
+request = requests.get(link)
+html_document = BeautifulSoup(request.text, "html.parser")
+all_paragraphs = html_document.find_all('p')
+
+print(f"Number of paragraph tags: {len(all_paragraphs)}") 
