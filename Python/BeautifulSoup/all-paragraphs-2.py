e56872484e9737ec7e789edd02f4653c7a1c8cc4
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-22 15:30:53 -0300

    feat: Solve exercise 2 from BeautifulSoup folder

--- Python/BeautifulSoup/all-paragraphs-2.py
+++ Python/BeautifulSoup/all-paragraphs-2.py
@@ -1,1 +1,11 @@
 # Write a Python program to retrieve all the paragraph tags from a given html document.
+
+import requests
+from bs4 import BeautifulSoup
+
+link = "https://www.linguanaturalis.com/"
+request = requests.get(link)
+html_document = BeautifulSoup(request.text, "html.parser")
+all_paragraphs = html_document.find_all('p')
+
+print(all_paragraphs) 
