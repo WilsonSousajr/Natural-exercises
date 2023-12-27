655afe5195be29b641b56b0669439450bdcb3d35
Wilson Sousa <willnapolitano21@gmail.com>
2023-12-27 16:10:47 -0300

    🔧(urls.py): Add debug_toolbar path to urlpatterns

--- Django/helloworld/helloworld/urls.py
+++ Django/helloworld/helloworld/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('helloworld_/', include("helloworld_.urls")),
     path('admin/', admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
